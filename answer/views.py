73f49dcbc7bd32d663ea61d93ba690ebb66322d4
yangjucai <1165456797@qq.com>
2023-06-03 16:12:26 +0800

    add tables

--- answer/views.py
+++ answer/views.py
@@ -1,7 +1,5 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from pyparsing import WordStart
-from answer.models import Questionanswer
 from django.db import connection
 import jieba
 import jieba.posseg as pseg
@@ -38,22 +36,51 @@ def answer_list(request):
     # for (word, flag) in pseg.cut(question):
     #     words_str += "'" + word + "',"
 
-    
+    hasBrand = False
+    hasType = False
+    brand = ""
+    type = ""
     words_str = words_str.strip(',')
+    words_new = words_str.split(',')
+    print(words_new)
+    for word in words_new:
+        with connection.cursor() as cursor:
+            cursor.execute("select * from brand where brandname = " + word +";")
+            result = cursor.fetchall()
+            if len(result) > 0:
+                hasBrand = True 
+                brand = word[1:len(word)-1]
+            cursor.execute("select * from type where typename = " + word +";")
+            result = cursor.fetchall()
+            if len(result) > 0:
+                hasType = True 
+                type = word[1:len(word)-1]
+        if hasBrand and hasType:
+            break 
 
+    nullAnswer = "感谢您的使用！您查询的产品目前没有收录到我们的资料里，抱歉！\n我们中国近现代工业产品设计智能问答目前主要收录新中国建立前后到1990年代前的代表性品牌和产品，我们的产品数据库正在继续完善中，敬请期待。\n如需进一步查询，请点击"
+    response = []
+    if hasBrand and hasType:
+        sql = "SELECT T1.id, question, answer, img, logo, pre \
+                FROM questionanswer as T1  join \
+                (SELECT qid, count(word) as cnt FROM wordquestion as T2 \
+                WHERE word IN (" + words_str + ") GROUP BY T2.qid ORDER BY cnt DESC limit 1) as T3 where T1.id = qid;"
 
-    sql = "SELECT T1.id, question, answer, img, logo, pre \
-            FROM questionanswer as T1  join \
-            (SELECT qid, count(word) as cnt FROM wordquestion as T2 \
-            WHERE word IN (" + words_str + ") GROUP BY T2.qid ORDER BY cnt DESC limit 1) as T3 where T1.id = qid"
+        print(sql)
 
-    print(sql)
-
-    with connection.cursor() as cursor:
-        cursor.execute(sql)
-        # cursor.execute("select * from questionanswer limit 10")
-        list = cursor.fetchall()
-    
-    response = [{"id": list[0][0], "question":list[0][1], "answer":list[0][2], "img":list[0][3], "logo": list[0][4], "pre": list[0][5]}]
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            # cursor.execute("select * from questionanswer limit 10")
+            list = cursor.fetchall()
+        
+        print(brand)
+        print(type)
+        if list[0][1].find(brand) == -1 or list[0][1].find(type) == -1:
+            print(list[0][1])
+            response = [{"id": -1, "question":question, "answer":nullAnswer, "img":"no image", "logo": "no logo", "pre": "no pre"}]
+        else:
+            response = [{"id": list[0][0], "question":list[0][1], "answer":list[0][2], "img":list[0][3], "logo": list[0][4], "pre": list[0][5]}]
+    else:
+        response = [{"id": -1, "question":question, "answer":nullAnswer, "img":"no image", "logo": "no logo", "pre": "no pre"}]
     
     return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii":False})
